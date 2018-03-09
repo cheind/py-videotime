dd3fea0fdbd10e95a72bf610d55020ef74c1ce66
Christoph Heindl <christoph.heindl@gmail.com>
2018-03-09 18:01:57 +0100

    support for writing result file

--- videotime/apps/extract.py
+++ videotime/apps/extract.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train time detector')
     parser.add_argument('model', help='Model file')
     parser.add_argument('input', help='Directory containing images and image pattern, or path to video')    
-    parser.add_argument('--verbose', action='store_true')  
+    parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--out', help='Name of resulting file', default='timestamps.txt')
     args = parser.parse_args()
 
     detector = vt.TimeDetector.load(args.model)
@@ -17,10 +18,17 @@ if __name__ == '__main__':
 
     timestamps = []
     while True:
-        b, img = cap.read()        
+        b, img = cap.read() # slowest part is decoding the frame  
         if not b:
             break
 
         dtime, prob = detector.detect(img)
         timestamps.append(dtime)
         print(dtime)
+
+    with open(args.out, 'w') as f: 
+        st = [str(t)+'\n' for t in timestamps]
+        f.writelines(st)
+
+
+    assert all(timestamps[i] <= timestamps[i+1] for i in range(len(timestamps)-1))
